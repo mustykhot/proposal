18953f018dd3edcd9379d15515973a95f8dedc51
rhammy <olalekanrahman06@gmail.com>
2024-10-03 12:40:49 +0100

    ft:Vote and events

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Box } from "@radix-ui/themes";
 import Layout from "./components/Layout";
 import CreateProposalModal from "./components/CreateProposalModal";
 import Proposals from "./components/Proposals";
@@ -8,78 +7,114 @@ import { Contract } from "ethers";
 import useRunners from "./hooks/useRunners";
 import { Interface } from "ethers";
 import ABI from "./ABI/proposal.json";
+import { Modal } from "./components/Modal";
 
 const multicallAbi = [
-    "function tryAggregate(bool requireSuccess, (address target, bytes callData)[] calls) returns ((bool success, bytes returnData)[] returnData)",
+  "function tryAggregate(bool requireSuccess, (address target, bytes callData)[] calls) returns ((bool success, bytes returnData)[] returnData)",
 ];
-
 function App() {
-    const readOnlyProposalContract = useContract(true);
-    const { readOnlyProvider } = useRunners();
-    const [proposals, setProposals] = useState([]);
+  const readOnlyProposalContract = useContract(true);
+  const { readOnlyProvider } = useRunners();
+  const [proposals, setProposals] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const fetchProposals = useCallback(async () => {
-        if (!readOnlyProposalContract) return;
+  const fetchProposals = useCallback(async () => {
+    if (!readOnlyProposalContract) return;
 
-        const multicallContract = new Contract(
-            import.meta.env.VITE_MULTICALL_ADDRESS,
-            multicallAbi,
-            readOnlyProvider
-        );
-
-        const itf = new Interface(ABI);
-
-        try {
-            const proposalCount = Number(
-                await readOnlyProposalContract.proposalCount()
-            );
-
-            const proposalsIds = Array.from(
-                { length: proposalCount - 1 },
-                (_, i) => i + 1
-            );
-
-            const calls = proposalsIds.map((id) => ({
-                target: import.meta.env.VITE_CONTRACT_ADDRESS,
-                callData: itf.encodeFunctionData("proposals", [id]),
-            }));
-
-            const responses = await multicallContract.tryAggregate.staticCall(
-                true,
-                calls
-            );
-
-            const decodedResults = responses.map((res) =>
-                itf.decodeFunctionResult("proposals", res.returnData)
-            );
-
-            const data = decodedResults.map((proposalStruct) => ({
-                description: proposalStruct.description,
-                amount: proposalStruct.amount,
-                minRequiredVote: proposalStruct.minVotesToPass,
-                votecount: proposalStruct.voteCount,
-                deadline: proposalStruct.votingDeadline,
-                executed: proposalStruct.executed,
-            }));
-
-            setProposals(data);
-        } catch (error) {
-            console.log("error fetching proposals: ", error);
-        }
-    }, [readOnlyProposalContract, readOnlyProvider]);
-
-    useEffect(() => {
-        fetchProposals();
-    }, [fetchProposals]);
-
-    return (
-        <Layout>
-            <Box className="flex justify-end p-4">
-                <CreateProposalModal />
-            </Box>
-            <Proposals proposals={proposals} />
-        </Layout>
+    const multicallContract = new Contract(
+      import.meta.env.VITE_MULTICALL_ADDRESS,
+      multicallAbi,
+      readOnlyProvider
     );
+
+    const itf = new Interface(ABI);
+
+    try {
+      const proposalCount = Number(
+        await readOnlyProposalContract.proposalCount()
+      );
+
+      const proposalsIds = Array.from(
+        { length: proposalCount - 1 },
+        (_, i) => i + 1
+      );
+
+      const calls = proposalsIds.map((id) => ({
+        target: import.meta.env.VITE_CONTRACT_ADDRESS,
+        callData: itf.encodeFunctionData("proposals", [id]),
+      }));
+
+      const responses = await multicallContract.tryAggregate.staticCall(
+        true,
+        calls
+      );
+
+      const decodedResults = responses.map((res) =>
+        itf.decodeFunctionResult("proposals", res.returnData)
+      );
+
+      const data = decodedResults.map((proposalStruct) => ({
+        description: proposalStruct.description,
+        amount: proposalStruct.amount,
+        minRequiredVote: proposalStruct.minVotesToPass,
+        votecount: proposalStruct.voteCount,
+        deadline: proposalStruct.votingDeadline,
+        executed: proposalStruct.executed,
+      }));
+
+      setProposals(data);
+    } catch (error) {
+      console.log("error fetching proposals: ", error);
+    }
+  }, [readOnlyProposalContract, readOnlyProvider]);
+
+  useEffect(() => {
+    fetchProposals();
+
+    const contract = new Contract(
+      import.meta.env.VITE_CONTRACT_ADDRESS,
+      ABI,
+      readOnlyProvider
+    );
+
+    contract.on("ProposalCreated", () => {
+      fetchProposals();
+    });
+
+    contract.on("Voted", () => {
+      fetchProposals();
+    });
+
+    return () => {
+      contract.removeAllListeners("ProposalCreated");
+      contract.removeAllListeners("Voted");
+    };
+  }, [fetchProposals, readOnlyProvider]);
+
+  return (
+    <Layout>
+      <div className="w-full flex my-5">
+        <button
+          className="bg-blue-500 p-4 text-white shadow-md rounded-md"
+          onClick={() => setIsModalOpen(true)}
+        >
+          Create Proposal
+        </button>
+      </div>
+      <Modal
+        handleClose={() => setIsModalOpen(false)}
+        isOpen={isModalOpen}
+        isSuccessModal
+        title="Create Proposal"
+      >
+        <CreateProposalModal
+          closeModal={() => setIsModalOpen(false)}
+          refetch={fetchProposals}
+        />
+      </Modal>
+      <Proposals proposals={proposals} />
+    </Layout>
+  );
 }
 
 export default App;
--- src/hooks/useCreateProposal.js
+++ src/hooks/useCreateProposal.js
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { toast } from "react-toastify";
 import useContract from "./useContract";
 import { useAppKitAccount } from "@reown/appkit/react";
@@ -6,69 +6,72 @@ import { useAppKitNetwork } from "@reown/appkit/react";
 import { liskSepoliaNetwork } from "../connection";
 import { parseEther } from "ethers";
 
-const useCreateProposal = () => {
-    const contract = useContract(true);
-    const { address } = useAppKitAccount();
-    const { chainId } = useAppKitNetwork();
-    return useCallback(
-        async (description, recipient, amount, duration, minVote) => {
-            if (
-                !description ||
-                !recipient ||
-                !amount ||
-                !duration ||
-                !minVote
-            ) {
-                toast.error("Missing field(s)");
-                return;
-            }
-            if (!address) {
-                toast.error("Connect your wallet!");
-                return;
-            }
-            if (Number(chainId) !== liskSepoliaNetwork.chainId) {
-                toast.error("You are not connected to the right network");
-                return;
-            }
+const useCreateProposal = (refetch, closeModal) => {
+  const contract = useContract(true);
+  const { address } = useAppKitAccount();
+  const { chainId } = useAppKitNetwork();
 
-            if (!contract) {
-                toast.error("Cannot get contract!");
-                return;
-            }
+  const [isLoading, setIsLoading] = useState(false);
 
-            try {
-                const estimatedGas = await contract.createProposal.estimateGas(
-                    description,
-                    recipient,
-                    parseEther(amount),
-                    duration,
-                    minVote
-                );
-                const tx = await contract.createProposal(
-                    description,
-                    recipient,
-                    parseEther(amount),
-                    duration,
-                    minVote,
-                    {
-                        gasLimit: (estimatedGas * BigInt(120)) / BigInt(100),
-                    }
-                );
-                const reciept = await tx.wait();
+  const createProposal = useCallback(
+    async (description, recipient, amount, duration, minVote) => {
+      if (!description || !recipient || !amount || !duration || !minVote) {
+        toast.error("Missing field(s)");
+        return;
+      }
+      if (!address) {
+        toast.error("Connect your wallet!");
+        return;
+      }
+      if (Number(chainId) !== liskSepoliaNetwork.chainId) {
+        toast.error("You are not connected to the right network");
+        return;
+      }
 
-                if (reciept.status === 1) {
-                    toast.success("Proposal Creation successful");
-                    return;
-                }
-                toast.error("Proposal Creation failed");
-                return;
-            } catch (error) {
-                console.error("error while creating proposal: ", error);
-                toast.error("Proposal Creation errored");
-            }
-        },
-        [address, chainId, contract]
-    );
+      if (!contract) {
+        toast.error("Cannot get contract!");
+        return;
+      }
+
+      try {
+        setIsLoading(true);
+        const estimatedGas = await contract.createProposal.estimateGas(
+          description,
+          recipient,
+          parseEther(amount),
+          duration,
+          minVote
+        );
+        const tx = await contract.createProposal(
+          description,
+          recipient,
+          parseEther(amount),
+          duration,
+          minVote,
+          {
+            gasLimit: (estimatedGas * BigInt(120)) / BigInt(100),
+          }
+        );
+        const reciept = await tx.wait();
+
+        if (reciept.status === 1) {
+          toast.success("Proposal Creation successful");
+          refetch();
+          closeModal();
+          return;
+        }
+        toast.error("Proposal Creation failed");
+        return;
+      } catch (error) {
+        console.error("error while creating proposal: ", error);
+        toast.error("Proposal Creation errored");
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [address, chainId, contract, refetch, closeModal]
+  );
+  return { createProposal, isLoading };
 };
 
 export default useCreateProposal;
